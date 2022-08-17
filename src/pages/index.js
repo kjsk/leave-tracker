b853dc4e1585af4d0cbc4353b0ada1ce98e1a144
Manoj Ponugtoi <jameslucky917@gmail.com>
2022-08-17 10:31:41 +0530

    intro logo added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,37 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState, useEffect } from "react"
 import Login from './Login'
 import SEO from "../components/seo"
+import logo1 from "../images/intro-logo.svg";
+import logo2 from "../images/intro-text.svg";
+import { navigate } from "gatsby";
 
 
 const IndexPage = () => {
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      navigate("/Login")
+    }, 5000)
+  }, [])
   return (
     <Fragment>
       <SEO title="Login" description="Gatsby is a React-based open source framework with performance, scalability and security built-in." keywords={['gatsby', 'react']} />
-      <Login />
+      {/* <Login /> */}
+      {
+        loading &&
+        <div id="loader">
+          <div id="image_container">
+            <div id="img1_cont">
+              <img src={logo1} alt="img" id="image" />
+            </div>
+            <img src={logo2} alt="img" id="image2" />
+          </div>
+        </div>
+      }
     </Fragment>
   )
 }
