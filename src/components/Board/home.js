aed103ac81d9916e17ffe406754bf3998200c8df
Manoj Ponugtoi <jameslucky917@gmail.com>
2022-09-02 11:19:49 +0530

    home cassual => casual

--- src/components/Board/home.js
+++ src/components/Board/home.js
@@ -1,100 +1,178 @@
-import React from 'react';
-import Moment from 'moment';
-import EmptyRoster from "../EmptyRoster";
+import React from "react"
+import Moment from "moment"
+import EmptyRoster from "../EmptyRoster"
 
 const Home = ({
-    RedoOutlined,
-    leaveApproved,
-    leavePending,
-    leaveRejected,
-    userLeaveData,
-    openLeaveDetailsFun,
-    desecision,
-    DeleteOutlined,
-    userDataMain,
-    getLeaves,
-    openNotificationWithIcon
+  RedoOutlined,
+  leaveApproved,
+  leavePending,
+  leaveRejected,
+  userLeaveData,
+  openLeaveDetailsFun,
+  desecision,
+  DeleteOutlined,
+  userDataMain,
+  getLeaves,
+  openNotificationWithIcon,
 }) => {
-    const calcLeaves = (24 - leaveApproved?.length);
-    return (
-        <>
-            <div id="score">
-                {userDataMain?.role === 'admin' ?
-                    <div id="score_card">
-                        <h2 id="score">{leavePending?.length < 10 ? `0${leavePending?.length}` : leavePending?.length}</h2>
-                        <p>Action Needed</p>
-                    </div>
-                    :
-                    <div id="score_card">
-                        <h2 id="score">{calcLeaves < 10 ? `0${calcLeaves}` : calcLeaves}</h2>
-                        <p>Available Leaves</p>
-                    </div>
-                }
-                <div id="score_card">
-                    <h2 id="score">{leaveApproved?.length < 10 ? `0${leaveApproved?.length}` : leaveApproved?.length}</h2>
-                    <p>Approved Leaves</p>
-                </div>
-                <div id="score_card">
-                    <h2 id="score">{leavePending?.length < 10 ? `0${leavePending?.length}` : leavePending?.length}</h2>
-                    <p>Pending Leaves Requests</p>
-                </div>
-                <div id="score_card">
-                    <h2 id="score">{leaveRejected?.length < 10 ? `0${leaveRejected?.length}` : leaveRejected?.length}</h2>
-                    <p>Rejected Leaves</p>
-                </div>
+  const calcLeaves = 24 - leaveApproved?.length
+  return (
+    <>
+      <div id="score">
+        {userDataMain?.role === "admin" ? (
+          <div id="score_card">
+            <h2 id="score">
+              {leavePending?.length < 10
+                ? `0${leavePending?.length}`
+                : leavePending?.length}
+            </h2>
+            <p>Action Needed</p>
+          </div>
+        ) : (
+          <div id="score_card">
+            <h2 id="score">
+              {calcLeaves < 10 ? `0${calcLeaves}` : calcLeaves}
+            </h2>
+            <p>Available Leaves</p>
+          </div>
+        )}
+        <div id="score_card">
+          <h2 id="score">
+            {leaveApproved?.length < 10
+              ? `0${leaveApproved?.length}`
+              : leaveApproved?.length}
+          </h2>
+          <p>Approved Leaves</p>
+        </div>
+        <div id="score_card">
+          <h2 id="score">
+            {leavePending?.length < 10
+              ? `0${leavePending?.length}`
+              : leavePending?.length}
+          </h2>
+          <p>Pending Leaves Requests</p>
+        </div>
+        <div id="score_card">
+          <h2 id="score">
+            {leaveRejected?.length < 10
+              ? `0${leaveRejected?.length}`
+              : leaveRejected?.length}
+          </h2>
+          <p>Rejected Leaves</p>
+        </div>
+      </div>
+
+      <p id="refresh" onClick={getLeaves} role="presentation">
+        <RedoOutlined />
+        Refresh
+      </p>
+
+      <div id="message">
+        {userLeaveData?.leaves?.length > 0 ? (
+          <>
+            <div id="message_block1">
+              <h3>SNo</h3>
+              <h3>Type</h3>
+              <h3>From</h3>
+              <h3>To</h3>
+              <h3>Reason</h3>
+              <h3>Status</h3>
+              <h3>Action</h3>
             </div>
-
-            <p id="refresh" onClick={getLeaves} role="presentation"><RedoOutlined />Refresh</p>
-
-            <div id="message">
-                {userLeaveData?.leaves?.length > 0 ?
-                    <>
-                        <div id="message_block1">
-                            <h3>SNo</h3>
-                            <h3>Type</h3>
-                            <h3>From</h3>
-                            <h3>To</h3>
-                            <h3>Reason</h3>
-                            <h3>Status</h3>
-                            <h3>Action</h3>
-                        </div>
-                        <div id="message_block2">
-                            {userLeaveData?.leaves?.map((item, i) => {
-
-                                return (
-                                    <div id="task_container" key={i}>
-                                        <p onClick={() => openLeaveDetailsFun(item, 'home')} role="presentation">{userLeaveData?.leaves?.length - i}</p>
-                                        <p style={{ padding: `0` }} onClick={() => openLeaveDetailsFun(item, 'home')} role="presentation">{item?.type === "gen" ? 'Paid' : 'Cassual'}</p>
-                                        <p onClick={() => openLeaveDetailsFun(item, 'home')} role="presentation">{Moment(item?.startDate, "MM-Do-YYYY").format("Do MMM YYYY - LT")}</p>
-                                        <p onClick={() => openLeaveDetailsFun(item, 'home')} role="presentation">{Moment(item?.endDate, "MM-Do-YYYY").format("Do MMM YYYY - LT")}</p>
-                                        <p onClick={() => openLeaveDetailsFun(item, 'home')} role="presentation">{item?.reason}</p>
-                                        <p style={{
-                                            color: item?.status === 'pending' ? '#CB5A08' :
-                                                item?.status === 'approved' ? '#00D241'
-                                                    :
-                                                    item?.status === 'rejected' ? '#FF0000' :
-                                                        '', fontWeight: `bold`
-                                        }} onClick={() => openLeaveDetailsFun(item, 'home')} role="presentation">
-                                            {item?.status === 'pending' ? 'Pending' :
-                                                item?.status === 'approved' ? 'Approved'
-                                                    :
-                                                    item?.status === 'rejected' ? 'Rejected' :
-                                                        ''}
-                                        </p>
-                                        <p onClick={() => { item?.status === 'pending' ? desecision('delete', item?.id) : openNotificationWithIcon('error', 'Not allowed to delete Approved or Rejected requests') }} role="presentation"><DeleteOutlined className={item?.status === 'pending' && 'delete_icon'} /></p>
-                                    </div>
-                                )
-                            }
-                            )}
-
-                        </div>
-                    </>
-                    :
-                    <EmptyRoster text={userDataMain?.role === 'admin' ? 'No action needed...' : 'No Leaves Applied...'} />
-                }
+            <div id="message_block2">
+              {userLeaveData?.leaves?.map((item, i) => {
+                return (
+                  <div id="task_container" key={i}>
+                    <p
+                      onClick={() => openLeaveDetailsFun(item, "home")}
+                      role="presentation"
+                    >
+                      {userLeaveData?.leaves?.length - i}
+                    </p>
+                    <p
+                      style={{ padding: `0` }}
+                      onClick={() => openLeaveDetailsFun(item, "home")}
+                      role="presentation"
+                    >
+                      {item?.type === "gen" ? "Paid" : "Casual"}
+                    </p>
+                    <p
+                      onClick={() => openLeaveDetailsFun(item, "home")}
+                      role="presentation"
+                    >
+                      {Moment(item?.startDate, "MM-Do-YYYY").format(
+                        "Do MMM YYYY - LT"
+                      )}
+                    </p>
+                    <p
+                      onClick={() => openLeaveDetailsFun(item, "home")}
+                      role="presentation"
+                    >
+                      {Moment(item?.endDate, "MM-Do-YYYY").format(
+                        "Do MMM YYYY - LT"
+                      )}
+                    </p>
+                    <p
+                      onClick={() => openLeaveDetailsFun(item, "home")}
+                      role="presentation"
+                    >
+                      {item?.reason}
+                    </p>
+                    <p
+                      style={{
+                        color:
+                          item?.status === "pending"
+                            ? "#CB5A08"
+                            : item?.status === "approved"
+                            ? "#00D241"
+                            : item?.status === "rejected"
+                            ? "#FF0000"
+                            : "",
+                        fontWeight: `bold`,
+                      }}
+                      onClick={() => openLeaveDetailsFun(item, "home")}
+                      role="presentation"
+                    >
+                      {item?.status === "pending"
+                        ? "Pending"
+                        : item?.status === "approved"
+                        ? "Approved"
+                        : item?.status === "rejected"
+                        ? "Rejected"
+                        : ""}
+                    </p>
+                    <p
+                      onClick={() => {
+                        item?.status === "pending"
+                          ? desecision("delete", item?.id)
+                          : openNotificationWithIcon(
+                              "error",
+                              "Not allowed to delete Approved or Rejected requests"
+                            )
+                      }}
+                      role="presentation"
+                    >
+                      <DeleteOutlined
+                        className={item?.status === "pending" && "delete_icon"}
+                      />
+                    </p>
+                  </div>
+                )
+              })}
             </div>
-        </>
-    )
+          </>
+        ) : (
+          <EmptyRoster
+            text={
+              userDataMain?.role === "admin"
+                ? "No action needed..."
+                : "No Leaves Applied..."
+            }
+          />
+        )}
+      </div>
+    </>
+  )
 }
 
-export default Home;
+export default Home
--- src/components/loader.js
+++ src/components/loader.js
@@ -1,15 +1,27 @@
 import React from "react"
 import { Spin } from "antd"
-import { LoadingOutlined } from '@ant-design/icons';
-
+import { LoadingOutlined } from "@ant-design/icons"
 
 const Loader = () => {
-    const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
-    return (
-        <div style={{ background: `#363740`, height: `100vh`, display: `flex`, justifyContent: `center`, alignItems: `center`, position: `fixed`, top: `0`, left: `0`, width: `100%`, zIndex: 100000 }}>
-            <Spin indicator={antIcon} />
-        </div>
-    )
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />
+  return (
+    <div
+      style={{
+        background: `#363740`,
+        height: `100vh`,
+        display: `flex`,
+        justifyContent: `center`,
+        alignItems: `center`,
+        position: `fixed`,
+        top: `0`,
+        left: `0`,
+        width: `100%`,
+        zIndex: 100000,
+      }}
+    >
+      <Spin indicator={antIcon} />
+    </div>
+  )
 }
 
 export default Loader
